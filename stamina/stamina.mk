44e6b18f79e495c216584c558e692d06abee566b
ahnet-69 <syedmusicrealise@gmail.com>
2024-11-03 12:13:48 +0000

    stamina: Update Implementation
    * Up to date with https://github.com/XperiaLabs/vendor_sony_extra/commit/dc719e03903771398c38386c30330c4225ae3b7f
    
    (saku pro asf sar.)

--- stamina/stamina.mk
+++ stamina/stamina.mk
@@ -19,32 +19,32 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/sony/extra/stamina
 
 # Local Path
-LOCAL_STAM := vendor/sony/extra/stamina
+LOCAL_STAMINA := vendor/sony/extra/stamina
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(LOCAL_STAM)/proprietary/vendor/etc/vintf/stamina_framework_matrix.xml
-
-DEVICE_MANIFEST_FILE += \
-    $(LOCAL_STAM)/proprietary/vendor/etc/vintf/vendor.somc.hardware.superstamina.manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(LOCAL_STAMINA)/proprietary/vendor/etc/vintf/manifest/superstamina_framework_matrix.xml
+DEVICE_MANIFEST_FILE += $(LOCAL_STAMINA)/proprietary/vendor/etc/vintf/manifest/vendor.somc.hardware.superstamina.xml
 
 # SEPolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(LOCAL_STAM)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(LOCAL_STAMINA)/sepolicy/vendor
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_STAM)/configs/perms/system_ext/etc/com.sonymobile.superstamina.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.sonymobile.superstamina.xml \
-    $(LOCAL_STAM)/configs/perms/system_ext/etc/privapp-com.sonymobile.superstamina.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.sonymobile.superstamina.xml
+    $(LOCAL_STAMINA)/proprietary/system_ext/etc/permissions/com.sonymobile.staminalevel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.sonymobile.staminalevel.xml \
+    $(LOCAL_STAMINA)/proprietary/system_ext/etc/permissions/com.sonymobile.superstamina.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.sonymobile.superstamina.xml \
+    $(LOCAL_STAMINA)/proprietary/system_ext/etc/sysconfig/config_com.sonymobile.superstamina.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config_com.sonymobile.superstamina.xml
 
-# Blobs
+# Files
 PRODUCT_COPY_FILES += \
-   $(LOCAL_STAM)/proprietary/vendor/bin/hw/vendor.somc.hardware.superstamina@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.somc.hardware.superstamina@1.0-service \
-   $(LOCAL_STAM)/proprietary/vendor/etc/init/vendor.somc.hardware.superstamina@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.somc.hardware.superstamina@1.0-service.rc \
-   $(LOCAL_STAM)/proprietary/vendor/lib64/vendor.somc.hardware.superstamina@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.somc.hardware.superstamina@1.0.so
+   $(LOCAL_STAMINA)/proprietary/vendor/bin/hw/vendor.somc.hardware.superstamina@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.somc.hardware.superstamina@1.0-service \
+   $(LOCAL_STAMINA)/proprietary/vendor/etc/init/vendor.somc.hardware.superstamina@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.somc.hardware.superstamina@1.0-service.rc \
+   $(LOCAL_STAMINA)/proprietary/vendor/lib64/vendor.somc.hardware.superstamina@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.somc.hardware.superstamina@1.0.so
 
 # Packages
 PRODUCT_PACKAGES += \
-	FrameworksRes-Stamina-Overlay \
-	SettingsRes-Stamina-Overlay \
-	SystemUIRes-Stamina-Overlay \
+	StaminaLevel \
+	SuperStamina \
+	FrameworksRes-StaminaMode-Overlay \
+	SettingsRes-StaminaMode-Overlay \
+	SystemUIRes-StaminaMode-Overlay \
 	com.sonymobile.superstamina_impl
